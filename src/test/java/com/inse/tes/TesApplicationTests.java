f97b6baf11f7387ed14326ce1714b33924057799
hengyangan <277265318@qq.com>
2020-02-01 20:40:28 -0500

    first init

---
+++ src/test/java/com/inse/tes/TesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inse.tes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
